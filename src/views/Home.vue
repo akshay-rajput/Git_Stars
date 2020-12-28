7c1c4369a5ffe9dbfc79818f431211a5c3108ba9
Akshay <akshayrajput102@gmail.com>
2020-12-28 19:08:17 +0530

    feat(Homepage): smooth scroll for show more/less repos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between items-center">
       <h3 class="text-xl">Popular JS Repositories</h3>
       <div class="">
-        <button class="btn-app border border-gray-50 px-3 py-1" @click="fetchRepos">Get Repo</button>
+        <!-- <button class="btn-app border border-gray-50 px-3 py-1" @click="fetchRepos">Get Repo</button> -->
         <button class="hidden sm:inline mx-3 md:mx-4 px-1 btn-display-cards" :class="showAsList ? '':'btn-toggled'" @click="showAsList=false">
           <span class="fa fa-border-all text-xl"></span>
         </button>
@@ -12,7 +12,16 @@
         </button>
       </div>
     </div>
-    <ShowRepos :repoList=fetchedRepoList :repoListDisplay="showAsList" />
+    
+    <div class="repo-list-parent">
+      <div class="repo-list-limited overflow-hidden" :style="{ maxHeight: repoListHeight }">
+        <ShowRepos :repoList=fetchedRepoList :repoListDisplay="showAsList" />
+      </div>
+      <div class="text-center repoList-displayOptions">
+        <button class="btn-showmore" v-if="showLess" @click="showMoreRepos" >Show more</button>
+        <button class="btn-showmore" v-if="showMore" @click="showLessRepos" >Show less</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,8 +39,14 @@ export default {
     return{
       showAsList: false,
       // showAsCard: true,
-      fetchedRepoList: []
+      fetchedRepoList: [],
+      repoListHeight: '750px',
+      showLess: true,
+      showMore: false
     }
+  },
+  mounted() {
+    this.fetchRepos();
   },
   methods: {
     fetchRepos() {
@@ -58,12 +73,24 @@ export default {
           return response.data;
         })
         .then((repolist) => {
-          console.log("repoList length: ", repolist.items);
+          console.log("repoList: ", repolist.items);
           this.fetchedRepoList = repolist.items;
           
           // console.log("type of repolist: ", repolist);
         });
     },
+    showMoreRepos(){
+      console.log("showing more");
+      this.showMore = true;
+      this.showLess = false;
+      this.repoListHeight = '5000px';
+    },
+    showLessRepos(){
+      console.log("showing less");
+      this.showLess = true;
+      this.showMore = false;
+      this.repoListHeight = '750px';
+    }
   }
 }
 </script>
@@ -77,5 +104,38 @@ export default {
       outline-color: teal;
     }
   }
+  .btn-showmore{
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+  }
 
+  .repo-list-parent{
+    position: relative;
+  }
+  .repo-list-limited{
+    // height: auto;
+    // max-height: 750px;
+    transition: max-height ease-in 1s;
+    margin-bottom: 10px;
+  }
+
+  #app.lighttheme .repoList-displayOptions::before{
+    animation-name: fadeOutTextDelay;
+    animation-duration: 2s;
+    background: linear-gradient(0deg, rgba(255,255,255,1) 35%, rgba(255,255,255,0.25) 100%);    
+  }
+  #app.darktheme .repoList-displayOptions::before{
+      background: linear-gradient(0deg, rgba(68,68,68,1) 35%, rgba(68,68,68,0.25) 100%);
+      animation-name: fadeInTextDelay;
+      animation-duration: 3s;
+  }
+  .repoList-displayOptions::before{
+    content: '';
+    width: 100%;
+    left: 0;
+    height: 2rem;
+    position: absolute;
+    bottom: 40px;
+    // transition: background ease 0.75s;
+  }
 </style>
