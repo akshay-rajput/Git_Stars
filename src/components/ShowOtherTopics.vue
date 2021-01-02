558ab726af29647c9a3e46dc3f22a4fe184a78a3
Akshay <akshayrajput102@gmail.com>
2021-01-02 18:45:41 +0530

    style(Homepage): Ui improvements to other-topic-tags

--- src/components/ShowOtherTopics.vue
+++ src/components/ShowOtherTopics.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="other-topics-tags border border-gray-500">
-      <h4 class="text-xl">Other topics</h4>
-      <div class="other-topic-list mb-2" v-for="(topiclist, category) in otherTopics" :key="category">
-          <h5 class="mb-1 text-lg">{{category}}</h5>
-          <div class="py-1 flex items-center flex-wrap">
-              <router-link :to="{ name: 'Topic', params: { topicname: topic }}" class="text-sm topic-tag" v-for="topic in topiclist" :key="topic">
-                  {{topic}}
-              </router-link>
-          </div>
+  <div class="other-topics-tags">
+      <h4 class="text-2xl text-gray-400 mb-2">Other topics</h4>
+      <div class="grid sm:grid-cols-3 my-2">
+        <div class="other-topic-list mb-2" v-for="(topiclist, category) in otherTopics" :key="category">
+            <h5 class="mb-1 text-sm text-gray-400">{{category}}</h5>
+            <div class="py-1 flex items-center flex-wrap">
+                <router-link :to="{ name: 'Topic', params: { topicname: topic }}" class="text-sm topic-tag" v-for="topic in topiclist" :key="topic">
+                    {{topic}}
+                </router-link>
+            </div>
+        </div>
       </div>
+      
   </div>
 </template>
 
@@ -31,14 +34,14 @@ export default {
                     'Carousel',  
                     'Charts', 
                     'Map',
-                    'Date picker',
+                    'Datepicker',
                     'Autocomplete',
-                    'Select dropdown',
+                    'Select',
                     'Rich Text editing',
                 ],
                 "Meta": [
-                    'Frontend Interview',
-                    'DS & Algorithms',
+                    'Interview',
+                    'Data Structures',
                     'UI design',
                     'Awesome Lists',
                     'Accessibility'
@@ -50,14 +53,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
     .topic-tag{
+        font-size: 13px;
+        letter-spacing: 0.05rem;
         cursor: pointer;
         padding: 4px 8px;
-        background: orange;
-        color: white;
-        border-radius: 20px;
+        border-radius: 5px;
         margin-right: 8px;
         margin-bottom: 8px;
+    }
+    
+    @media(min-width: 768px) {
+        .other-topic-list:nth-child(2){
+            padding-left: 15px;
+            padding-right: 15px;
+            margin-left: 15px;
+            margin-right: 15px;
+            border-left: 1px dashed #aaa;
+            border-right: 1px dashed #aaa;
+        }
+    }
+
+    @media(max-width: 767px) {
+        .other-topic-list:nth-child(2){
+            padding-top: 10px;
+            padding-bottom: 10px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            border-top: 1px dashed #aaa;
+            border-bottom: 1px dashed #aaa;
+        }
     }
 </style>
