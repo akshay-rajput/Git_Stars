73100553900df2cb093fd4b08b2789b18498113b
Akshay <akshayrajput102@gmail.com>
2021-01-03 23:06:32 +0530

    feat(app): Added 404 page.

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center">
-      <h1 class="">404 error page not found</h1>
+  <div class="text-center flex justify-center items-center h-full w-full">
+      <h1 class="text-2xl">404 error page not found</h1>
   </div>
 </template>
 
