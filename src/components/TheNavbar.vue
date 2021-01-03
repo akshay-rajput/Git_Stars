247119aad987466fa23ef8127a530860a14f12f1
Akshay <akshayrajput102@gmail.com>
2021-01-03 21:56:48 +0530

    fix(Navbar): Change site name

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="fixed z-10 w-full">
         <div class="container mx-auto px-4 py-3 text-sm sm:text-lg flex justify-between items-center">
-            <router-link to="/" class="">fe_Repos</router-link>
+            <router-link to="/" class="">fe_TopRepos</router-link>
             <div class="">
-                <a href="https://github.com/Root-0/Git_Stars" class="mr-3 md:mr-4" title="Github Repository">
+                <a href="https://github.com/Root-0/Git_Stars" target="_blank" rel="noopener" class="mr-3 md:mr-4" title="Github Repository">
                     <span class="fa fa-code-branch"></span> Fork
                 </a>
                 <a href="javascript:void(0);" @click="darkmode=!darkmode" title="toggle theme">
