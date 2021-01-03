26a98a02d041438a980d82e03ce74e7177bffede
Akshay <akshayrajput102@gmail.com>
2021-01-03 21:43:43 +0530

    feat(App): Store loadingRepos in state

--- src/store/index.js
+++ src/store/index.js
@@ -7,23 +7,30 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     fetchedRepoList: [],
-    showAsList: false
+    showAsList: false,
+    loadingRepos: false
   },
   mutations: {
     mut_fetchedRepos(state, repolist){
       console.log("mutating");
       state.fetchedRepoList = repolist;
+    },
+    mut_loadingRepos(state, isRepoListLoaded){
+      console.log("loading repos");
+      state.loadingRepos =  isRepoListLoaded;
     }
   },
   actions: {
     action_fetchRepos({commit}, fe_topic) {
       // const queryString = 'q=' + encodeURIComponent('GitHub Octocat in:readme user:defunkt');
       // const querystring = 'q=topic:ruby+topic:rails'
-      // const queryString = 'q=sort=forks&direction=desc'
-      console.log("Action Topic: ", fe_topic);
-      const queryString = 'q='+fe_topic+'&sort=stars'
+      // const queryString = 'q=sort=forks&direction=desc'      
       // old query --> 'q='+fe_topic+'+topic:'+ fe_topic +'&sort=stars'
       // const queryString = 'q=javascript+is:featured&sort=stars'
+      
+      commit('mut_loadingRepos', true);
+
+      const queryString = 'q='+fe_topic+'&sort=stars'
       axios
         .get("https://api.github.com/search/repositories?"+queryString,{ 
               headers: {
@@ -36,18 +43,21 @@ export default new Vuex.Store({
           axios.get("https://api.github.com/rate_limit").then((response) => {
             console.log("ratelimit: ", response.data.resources.search);
           });
-
           return response.data;
         })
         .then((repolist) => {
           commit('mut_fetchedRepos', repolist.items); 
-          console.log("commiting");
-        });
+        }).then(() => {
+          commit('mut_loadingRepos', false);
+        })
     },
   },
   getters: {
     getter_fetchedRepos: state=> {
       return state.fetchedRepoList;
+    },
+    getter_loadingRepos: state=> {
+      return state.loadingRepos;
     }
   }
 })
