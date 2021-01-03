0e0a4ea02dd68b0df2a771d61bd85c4fa65c0e74
Akshay <akshayrajput102@gmail.com>
2021-01-03 22:32:34 +0530

    style(Navbar): Change name of site

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,13 +1,15 @@
 <template>
     <nav class="fixed z-10 w-full">
         <div class="container mx-auto px-4 py-3 text-sm sm:text-lg flex justify-between items-center">
-            <router-link to="/" class="">fe_TopRepos</router-link>
+            <router-link to="/" class="">
+                <span class="fab fa-github-alt"></span> TopRepos
+            </router-link>
             <div class="">
                 <a href="https://github.com/Root-0/Git_Stars" target="_blank" rel="noopener" class="mr-3 md:mr-4" title="Github Repository">
                     <span class="fa fa-code-branch"></span> Fork
                 </a>
                 <a href="javascript:void(0);" @click="darkmode=!darkmode" title="toggle theme">
-                    <span :class="!darkmode ? 'fa fa-moon': 'fa fa-sun'"></span>
+                    <span :class="!darkmode ? 'fa fa-sun': 'fa fa-moon'"></span>
                 </a>
             </div>
         </div>
